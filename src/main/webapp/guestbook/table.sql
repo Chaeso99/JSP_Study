ee8b69cd6b66780999c7bff6ca8142792628da57
Chaeso99 <storymmm@naver.com>
2023-03-17 16:37:55 +0900

    23.03.17

--- src/main/webapp/guestbook/table.sql
+++ src/main/webapp/guestbook/table.sql
@@ -3,18 +3,18 @@ create table tblJoin(
 	pwd char(20) not null,
 	name char(20) not null,
 	email char(30),
-	hp char(40), --홈페이지
-	grade char(2) default '0' --일반 사용자와 admin 계정을 구분하기 위한 값. admin은 1임
+	hp char(40),
+	grade char(2) default '0'
 );
 
-create table tblGuestBook( --방명록
+create table tblGuestBook(
 	num int primary key auto_increment,
-	id char(20) not null, --누가 썼는지(원래는 tblJoin과 FK연결해야함)
+	id char(20) not null,
 	contents text,
-	ip char(15) not null, --ip주소는 필수로 받아와야함(클라이언트의 ip임)
+	ip char(15) not null,
 	regdate date,
 	regtime datetime,
-	secret char(2) default '0' --일반글은 0, 비밀글은 1
+	secret char(2) default '0'
 );
 
 create table tblComment(
